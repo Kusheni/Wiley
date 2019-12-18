4718561aa4532ca008370849262fbea89c335687
Tharushika <ktharushik@wiley.com>
2019-12-18 15:23:50 +0530

    Third Commit

---
+++ src/hello.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 17 11:46:04 2019
+
+@author: ktharushik
+"""
+
+class HelloClass:
+    def sayhello(self):
+        print("Hello World")
+        return True
