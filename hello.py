25170f84071de52feb48e9eee49e0b14110f9de9
Tharushika <ktharushik@wiley.com>
2019-12-23 18:47:32 +0530

    Restructure 1

--- hello.py
+++ hello.py
@@ -25,32 +25,3 @@ class HelloClass:
         while(count<3):
             count=count+1
             print("New Test")
-=======
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Dec 17 11:46:04 2019
-
-@author: ktharushik
-"""
-
-class HelloClass:
-    def sayhello(self):
-        print("Hello World")
-        return True
-
-    def saybye(self):
-        print("Bye")
-        return True
-
-
-    def mango(self):
-        print("Bye")
-        return True
-
-    def count(self):
-        count=0
-        while(count<3):
-            count=count+1
-            print("New Test")
->>>>>>> c2671869d369778c12b9e9299fa2cebc2a44797d
-            return True
