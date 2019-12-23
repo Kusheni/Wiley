c2671869d369778c12b9e9299fa2cebc2a44797d
Kusheni <32046550+Kusheni@users.noreply.github.com>
2019-12-23 18:35:34 +0530

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 17 12:03:27 2019
+
+@author: ktharushik
+"""
+
---
+++ hello.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 17 11:46:04 2019
+
+@author: ktharushik
+"""
+
+class HelloClass:
+    def sayhello(self):
+        print("Hello World")
+        return True
+
+    def saybye(self):
+        print("Bye")
+        return True
+
+
+    def mango(self):
+        print("Bye")
+        return True
+
+    def count(self):
+        count=0
+        while(count<3):
+            count=count+1
+            print("New Test")
+            return True
